e93f2ea4a7290882cf213aa3423ebcc073afab88
geekvig <anandpadman91@gmail.com>
2025-05-20 12:50:24 -0700

    Adds MLflow support and DVClive

--- src/models/model_eval.py
+++ src/models/model_eval.py
@@ -1,10 +1,16 @@
 import numpy as np
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pickle
 import json
 
+from dvclive import Live
+import yaml
 from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
+from sklearn.metrics import confusion_matrix
+import mlflow
+
 
 def load_data(filepath : str) -> pd.DataFrame:
     try:
@@ -36,12 +42,43 @@ def load_model(filepath:str):
 #model = pickle.load(open("model.pkl","rb"))
 def evaluation_model(model, X_test:pd.DataFrame, y_test:pd.Series) -> dict:
     try:
+
+        params = yaml.safe_load(open("params.yaml", "r"))
+        test_size = params["data_collection"]["test_size"] 
+        n_estimators = params["model_building"]["n_estimators"]
         y_pred = model.predict(X_test)
 
         acc = accuracy_score(y_test,y_pred)
         pre = precision_score(y_test,y_pred)
         recall = recall_score(y_test,y_pred)
         f1score = f1_score(y_test,y_pred)
+
+        with mlflow.start_run():
+            mlflow.log_metric("accuracy", acc)
+            mlflow.log_metric("precision", pre)
+            mlflow.log_metric("recall", recall)
+            mlflow.log_metric("f1_score", f1score)
+            mlflow.log_param("test_size", test_size)
+            mlflow.log_param("n_estimators", n_estimators)
+            cm = confusion_matrix(y_test, y_pred)
+            plt.figure(figsize=(5,5))
+            sns.heatmap(cm, annot=True)
+            plt.xlabel("Predicted")
+            plt.ylabel("Actual")
+            plt.title("Confustion Matrix")
+            plt.savefig("confusion_matrix.png")
+            mlflow.log_artifact("confusion_matrix.png")
+            mlflow.sklearn.log_model(model, "RandomForest")
+
+
+        #Experiment tracking
+        with Live(save_dvc_exp=True) as live:
+            live.log_metric("accuracy", acc)
+            live.log_metric("precision", pre)
+            live.log_metric("recall", recall)
+            live.log_metric("f1_score", f1score)
+            live.log_param("test_size", test_size)
+            live.log_param("n_estimators", n_estimators)
 
         metrics_dict = {
 
